47321d0dd84ac62e82d2ba69d046d339dc27ef43
NORDESTIN0 <fcgabriel.coelho@gmail.com>
2022-07-29 16:08:12 -0300

    Update 1.5

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,30 +1,28 @@
 import React from "react";
-//import StarIcon from '@mui/icons-material/Star';
-//import StarHalfIcon from '@mui/icons-material/StarHalf';
-//import StarBorderIcon from '@mui/icons-material/StarBorder';
 import {Movie, Movielist} from "./styled"
 import { useState, useEffect } from "react";
-import { ResetTvOutlined } from "@mui/icons-material";
+//import { ResetTvOutlined } from "@mui/icons-material";
 
 function Home() {
 
 const [movies, setMovies] = useState([])
+const image_path = 'https://image.tmdb.org/t/p/w500'
 
 useEffect(() => {
     fetch(`https://api.themoviedb.org/3/movie/popular?api_key=e8fa4eae0c5122fbf1082350028a12f7&language=en-US&page=1`)
     .then (response => response.json())
-    .then (data => setMovies(data.results))
+    .then (data => {
+        console.log(data.results)
+        setMovies(data.results)})
 },[])
-
-
 
 return(
     <Movielist>
         {movies.map(movie => {
             return(
                 <Movie key= {movie.id}>
-                    <a href="#"><img src={movie.image_url}></img></a>
-                    <span>{movie.title}</span>
+                <a href="#"><img src={`${image_path}${movie.poster_path}`}></img></a>
+                <span>{movie.title}</span>
                 </Movie>
             )
         })};
